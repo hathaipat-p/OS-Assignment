8f738870cbcec5f25bb6ced36e579b7ce599403f
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-08-30 20:22:03 +0700

    edit 3_13.c

--- 3_13.c
+++ 3_13.c
@@ -11,7 +11,8 @@ int main() {
 
     if (pid < 0) { 
         /* error occurred */ 
-        fprintf(stderr, "Fork Failed"); return 1;
+        fprintf(stderr, "Fork Failed"); 
+        return 1;
     }
     else if (pid == 0) { 
         /* child process */ 
