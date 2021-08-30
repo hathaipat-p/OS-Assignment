20bd4bbd8ac368cf943501c0d4f3109f5a8a29bd
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-08-30 22:00:21 +0700

    program for exercise 4.19

---
+++ 4_19.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/types.h> 
+#include <sys/wait.h>
+#include <unistd.h>
+
+int value = 0;
+void *runner(void *param);  /* the thread */
+
+int main(int argc, char *argv[]) {
+    pid_t pid;
+    pthread_t tid; 
+    pthread_attr_t attr;
+
+    pid = fork();
+
+    if (pid == 0) { 
+        /* child process */ 
+        pthread_attr_init(&attr);
+        pthread_create(&tid,&attr,runner,NULL); 
+        pthread_join(tid,NULL);
+        printf("CHILD: value = %d\n",value); /* LINE C */
+    }
+    else if (pid > 0) { 
+        /* parent process */ 
+        wait(NULL);
+        printf("PARENT: value = %d\n",value); /* LINE P */
+    }
+}
+
+void *runner(void *param) { 
+    value = 5;
+    pthread_exit(0);
+}
