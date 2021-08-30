2558e583b5a467c49d5c41df26edf097481803fc
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-08-30 18:41:23 +0700

    program for question 3.11

---
+++ 3_11.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <unistd.h>
+
+int main() {
+    int i;
+
+    for (i = 0; i < 4; i++)
+        fork();
+
+    printf("process id: %d\n", (int)getpid());
+
+    return 0;
+}
